c9acb6b3cfc6f53f513e0491f83a38d5579e3d9f
cuixikun <2568039623@qq.com>
2016-06-07 00:46:44 +0800

    seven

---
+++ ABC/src/cui/string/Shuzu.java
@@ -0,0 +1,14 @@
+package cui.string;
+
+public class Shuzu {
+	public static void main(String[] args){
+		int data[]=new int[3];
+		data[0]=10;
+		data[1]=20;
+		data[2]=30;
+		for(int x=0;x<data.length;x++){
+			System.out.println(data[x]);
+		}
+	}
+
+}
